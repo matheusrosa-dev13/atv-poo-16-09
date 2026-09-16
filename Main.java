c5e30a13fe822dc64620fc3dbe7d5e62ee2e8009
matheusrosa-dev13 <mr8615077@gmail.com>
2026-09-16 09:06:43 -0300

    Add main method with Funcionario and Gerente instances

---
+++ Main.java
@@ -0,0 +1,8 @@
+void main(){
+    Funcionario f1 = new Funcionario();
+    Funcionario f2 = new Funcionario("caio",123, 1500);
+
+            Gerente Funcionario = new Gerente("Matheus",321, 5000," teste", 1000);
+
+
+}
